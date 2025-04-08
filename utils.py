46face795e6b7286eccbba1dfb833530ec1fac0c
Sanji <147115096+sanjixthapa@users.noreply.github.com>
2025-04-08 07:17:05 -0400

    Update utils.py
    added more common passwords

--- utils.py
+++ utils.py
@@ -9,6 +9,19 @@ SPECIAL = string.punctuation
 ALL_CHARS = LOWERCASE + UPPERCASE + DIGITS + SPECIAL
 
 COMMON_PATTERNS = [
-    '123456', 'password', 'qwerty', '111111',
-    'abc123', 'password1', 'admin', 'welcome'
+    # Very common numbers
+    '123456', '123456789', '12345678', '12345', '1234567', '111111', '000000',
+    # Default / generic passwords
+    'password', 'password1', 'admin', 'welcome', 'letmein', 'guest', 'test',
+    # Keyboard patterns
+    'qwerty', 'qwerty123', 'asdfgh', 'zxcvbnm', '1q2w3e4r', '123qwe',
+    # Names and phrases
+    'iloveyou', 'monkey', 'dragon', 'sunshine', 'football', 'baseball',
+    # Years and dates
+    '2020', '2021', '2022', '2023', '2024', '1984', '1990', '2000',
+    # Local and CS-related patterns
+    'oswego', 'laker', 'suny123', 'csc123', 'csc_101', 'csc_212', 'student', 'college',
+    # Extra generics
+    'abcd1234', 'welcome123', 'changeme', 'default', 'user', 'mypassword', 'superman'
 ]
+
